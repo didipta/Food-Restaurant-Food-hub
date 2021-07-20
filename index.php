7e28975f4aa7ff20fd6c68a107fddd5c8ffd617c
didipta <66532706+didipta@users.noreply.github.com>
2021-07-20 15:47:22 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/indexpage.css">
+    <link rel="icon" href="img/LOGO.jpg" type="image/x-icon" >
+    <title>Food-hub</title>
+</head>
+<body>
+    <div class="resta-logo">
+    <img src="img/LOGO.jpg" alt="">
+    </div>
+    <section id="bar">
+        <div class="logo">
+            <h3>Food-<span style="color: aliceblue;">Hub</span></h3>
+            <div class="nav">
+                <nav>
+                    <ul>
+                        <li> <a href="#">SPECIAL-MENU</a></li>
+                        <li><a href="#">FOODS</a></li>
+                        <li><a href="views/Aboutus.php">About Us</a></li>
+                        <li><a href="views/Contract.php">CONTACT US</a></li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </div>
+ 
+    </section>
+    
+   <section id="design">
+       <div class="des">
+          <h3>ONE-STOP</h3>
+          <h1>DELICIOUS</h1>
+          <h5>FOODS PLACE.</h5>
+          <p>One stop place for to lorem ipsum dolor sit amet
+            consectuere asep adisspiscing set diam like heaven one stop place
+            for you to larem ipsum dolor sit amet comsectuere adispiscing.</p>
+       </div>
+       <div class="log">
+        <a href="views/login.php">
+            <button class="btn">
+                LOG-IN
+            </button>
+        </a>
+    </div>
+   </section>
+   <div class="first-social icon">
+   <a href="01919160373"><i class="fa fa-phone" aria-hidden="true"></i></a>
+   <a href="http://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+   <a href="sdipta707@gmail.com"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+
+   </div>
+
+   <div class="second-social icon">
+    <a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i><br></a>
+    <a href="https://www.instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i><br>
+    <a href="https://www.whatsapp.com/"><i class="fa fa-whatsapp" aria-hidden="true"></i>
+   </div> 
+
+</body>
+</html>
